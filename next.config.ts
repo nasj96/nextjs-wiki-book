6356e7c14c5a0c371d457a2b33f4fc9c96e8e9a0
danielna <nasj96@naver.com>
2025-04-01 23:41:09 +0900

    styled-components 설정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  compiler: {
+    // stylecComponents 활성화
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
